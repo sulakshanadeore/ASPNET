18c38841475a03cebb03458812fa80bc3a5e9599
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-01-29 11:57:29 +0530

    Add files via upload

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FirstWebApp
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+               Response.Write("Page Loaded First time");
+            }
+            else
+            {
+                Response.Write("Page Loaded back as the button was clicked");
+            }
+        }
+
+        //protected event  void DelName(object sender, EventArgs e) Click;
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //Response.Write("<script>alert('Hello')</script>");
+            //embedding js in asp.net
+
+            
+        }
+    }
+}
