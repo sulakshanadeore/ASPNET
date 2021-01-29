20b131c601ba7fe15c63269a326efe07c0739078
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-01-29 15:37:05 +0530

    Add files via upload

---
+++ dbo.ValidateUserLogin.sql
@@ -0,0 +1,26 @@
+﻿CREATE FUNCTION [dbo].ValidateUserLogin
+(
+	@username varchar(20),
+	@pwd varchar(20)
+)
+RETURNS bit
+AS
+
+BEGIN
+declare
+@v_uname varchar(50),
+@v_pwd varchar(50),
+@v_status bit
+
+select @v_uname=username,@v_pwd=password from UserData
+where username=@username ;
+if (@pwd=@v_pwd)
+begin
+set @v_status=0
+end
+else 
+begin
+set @v_status=1
+end
+	return @v_status;
+END
