616ad57aba7b13f75af55f5d6bec3e13be9d20e2
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-02-04 10:33:07 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemoForPartialViewsValidations.Controllers
+{
+    public class HomeController : Controller
+    {
+        [ChildActionOnly]
+        public ActionResult Greeting()
+        {
+            //ViewBag.Data = "this is how u use partial views";
+            return PartialView("_GreetingView");
+        }
+
+        public ActionResult CustomerHome()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CustomerHome(string data)
+        {
+            string s=doSomething(data);
+            ViewBag.ReturnedData = s;
+            return View();
+        }
+
+        [NonAction]
+        public string doSomething(string data)
+        {
+           return data.Length.ToString();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
